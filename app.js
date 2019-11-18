a82f6c58a2efa198d11bb076af9dfcf33bf09f47
mayankkanela <mayankkanela8@gmail.com>
2019-11-18 16:10:40 +0530

    final commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let cells = document.querySelectorAll('.cell');
 let body =document.querySelector('body');
-//let clickable=false;
+let gameOver=false;
+let board= document.querySelector('#board');
 
 
 cells.forEach(function(cell,index)
@@ -15,7 +16,10 @@ let value="";
 function cellClicked(index)
 {
 return function(e){
-    
+    if(gameOver)
+    {  
+        return;
+    }
     if(i%2==0){
         e.target.textContent="O";
          zero+=index+1;
@@ -45,15 +49,39 @@ function checkwinner(player,value)
             console.log(true);
             let h2 =document.querySelector('h2');
             h2.textContent="O wins..click anywhere to reset";
-           // clickable=true;
-           body.addEventListener("click",function(){
-        reload();   
+           gameOver=true;
+         board.addEventListener("click",function(e){
+             console.log(e.target);
+            if(e.target==board)
+            reload();
+              
         }); 
            break;
         }
     }
+    else
+    if(player=='X')
+    {  
+        for(var j=0;j<winConditions.length;j++)
+        if(value.includes(winConditions[j])||value.includes(winConditions[j].split('').reverse().join('')))
+           {
+               console.log(true);
+               let h2 =document.querySelector('h2');
+               h2.textContent="X wins..click anywhere outside box to reset";
+               gameOver=true;  
+           
+              board.addEventListener("click",function(e){
+                   if(e.target==board)
+                    reload();
+              
+            }); 
+              break;
+           }
+       }
+       
 
 }
+
 function reload(){
     window.location.reload();
 }
